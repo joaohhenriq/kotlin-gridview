2c3851facaac9dd97d81992413ed4abc18049d29
joaohhenriq <joao_camargo10@hotmail.com>
2021-03-17 21:16:03 -0300

    First commit

---
+++ app/src/main/java/com/joaohhenriq/kotlin_gridview/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.joaohhenriq.kotlin_gridview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
