6ed354489afd2a00fdd97aa18e53b025a78ea460
joaohhenriq <joao_camargo10@hotmail.com>
2021-03-17 21:34:16 -0300

    Creating item design

--- app/src/main/java/com/joaohhenriq/kotlin_gridview/item_model.kt
+++ app/src/main/java/com/joaohhenriq/kotlin_gridview/item_model.kt
@@ -1,2 +1,17 @@
 package com.joaohhenriq.kotlin_gridview
 
+class ItemModel {
+    var name: String
+    var des: String
+    var image: Int
+
+    constructor(
+        name: String,
+        des: String,
+        image: Int
+    ) {
+        this.name = name
+        this.des = des
+        this.image = image
+    }
+}
